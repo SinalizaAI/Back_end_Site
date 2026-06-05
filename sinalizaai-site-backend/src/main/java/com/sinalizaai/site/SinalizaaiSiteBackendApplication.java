5912ae9c6057e2df8ca52b7e05865802b48be00c
KaueSiqueira <kaue54150@gmail.com>
2026-06-05 17:57:24 -0300

    inico do backend

---
+++ sinalizaai-site-backend/src/main/java/com/sinalizaai/site/SinalizaaiSiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sinalizaai.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinalizaaiSiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinalizaaiSiteBackendApplication.class, args);
+	}
+
+}
